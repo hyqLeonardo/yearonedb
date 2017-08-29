749a51c903ad2d75c5dd81306d1c6479e588ddaf
hyqLeonardo <hyq335335@163.com>
2017-08-29 23:31:51 +0800

    robust of dataframe shape inconsistence; get_factor resemble get_price

--- yearonedb/factor_db.py
+++ yearonedb/factor_db.py
@@ -7,8 +7,6 @@ import pandas as pd
 import datetime
 
 engine = create_engine('mysql+mysqldb://yearone:yearone@localhost:5432/factor_pool', echo=False)
-conn = engine.connect()
-metadata = MetaData(conn)
 
 
 def save_factor(factor_df, factor_name, save_mode='APPEND'):
@@ -23,8 +21,14 @@ def save_factor(factor_df, factor_name, save_mode='APPEND'):
     if save_mode not in ['APPEND', 'REPLACE']:
         print('save_mode is either \"APPEND\" or \"REPLACE\"')
         return
+    # get current metadata
+    conn = engine.connect()
+    metadata = MetaData(conn)
+    # make a copy to modify, or the calling of method upon original df will go mad
+    factor_df_copy = pd.DataFrame.copy(factor_df)
 
     if save_mode == 'APPEND':
+
         # check if column has changes
         try:
             t = Table(factor_name, metadata, autoload=True)
@@ -33,41 +37,46 @@ def save_factor(factor_df, factor_name, save_mode='APPEND'):
             return
 
         col_db = [m.key for m in t.columns]
-        col_df = factor_df.columns.values
+        col_db.remove('index_col')
+        col_df = list(factor_df_copy.columns.values)
         col_intercept = list(set(col_db) & set(col_df))
 
-        if len(col_db) == len(col_df):
-            if len(col_intercept) == len(col_db):   # no column changes
-                # get rid of repeated rows by making index unique
-                index_db = pd.read_sql_table(factor_name, columns=['index'], con=engine)
-                index_db = list(index_db['index'])
-                index_df = list(factor_df.index)
-                index_intercept = list(set(index_db) & set(index_df))
-                index_unique = list(set(index_df) - set(index_intercept))
-                index_unique.sort()
-                factor_df = factor_df.loc[index_unique, :]
-                # append with index=False, cause the column named as 'index'
-                # (which is auto assigned by index=True) conflict with SQL key word
-                factor_df['index_col'] = factor_df.index
-                factor_df.to_sql(name=factor_name, con=engine, if_exists='append', index=False)
+        if len(col_intercept) == len(col_df):  # no col changed, just append
+            # get rid of repeated rows by making index unique
+            index_db = pd.read_sql_table(factor_name, columns=['index_col'], con=engine)
+            index_db = [stamp.date() for stamp in index_db['index_col']]
+            index_df = list(factor_df_copy.index)
+            index_intercept = list(set(index_db) & set(index_df))
+            index_unique = list(set(index_df) - set(index_intercept))
+            index_unique.sort()
+            factor_df_copy = factor_df_copy.loc[index_unique, :]
 
-        assert len(col_intercept) < len(col_db)
-        # column changed, replace the DataFrame in Database
-        db_df = pd.read_sql('SELECT * from {}'.format(factor_name),
-                            con=engine, index_col=['index_col'])    # read all
-        db_df.index.name = 'index'  # from 'index_col' to 'index'
-        # make column of DataFrame in database consistent with factor_df
-        df = db_df[col_intercept]
-        new_col_name = list(set(col_df) - set(col_intercept))
-        for c in new_col_name:
-            df[c] = np.nan
-        save_factor(df, factor_name, 'REPLACE')
-        # append as usual
-        save_factor(factor_df, factor_name, 'APPEND')
+            # append with index=False, cause the column named as 'index'
+            # (which is auto assigned by index=True) conflict with SQL key word
+            factor_df_copy.loc[:, 'index_col'] = factor_df_copy.index
+            # append the original factor_df
+            factor_df_copy.to_sql(name=factor_name, con=engine, if_exists='append', index=False)
+            return
+
+        else:  # column changed, some removed, some append
+
+            # replace the DataFrame in Database with new columns
+            db_df = pd.read_sql('SELECT * from {}'.format(factor_name),
+                                con=engine, index_col=['index_col'])  # read all
+            db_df.index.name = 'index'  # from 'index_col' to 'index'
+            # make column of DataFrame in database consistent with factor_df
+            df = db_df.loc[:, col_intercept]
+            new_col_names = list(set(col_df) - set(col_intercept))
+            for c in new_col_names:
+                df.loc[:, c] = np.nan
+            save_factor(df, factor_name, 'REPLACE')
+            # append as usual
+            save_factor(factor_df, factor_name, 'APPEND')  # append the original factor_df
+            return
 
     if save_mode == 'REPLACE':
-        factor_df['index_col'] = factor_df.index
-        factor_df.to_sql(name=factor_name, con=engine, if_exists='replace', index=False)
+        factor_df_copy['index_col'] = factor_df_copy.index
+        factor_df_copy.to_sql(name=factor_name, con=engine, if_exists='replace', index=False)
 
 
 def get_factor(factor_name, order_book_ids, start_date, end_date=None):
@@ -79,9 +88,21 @@ def get_factor(factor_name, order_book_ids, start_date, end_date=None):
     :param end_date:        string of end date
     :return:    factor DataFrame
     """
-    if end_date is None:    # set today as end date
+    # get current metadata
+    conn = engine.connect()
+    metadata = MetaData(conn)
+    # get DataFrame's column in database
+    try:
+        t = Table(factor_name, metadata, autoload=True)
+    except sqlalchemy.exc.NoSuchTableError as ex:
+        print(ex)
+        return
+    col_db = [m.key for m in t.columns]
+    col_intercept = list(set(col_db) & set(order_book_ids))
+
+    if end_date is None:  # set today as end date
         end_date = str(datetime.date.today())
-    cols = 'index_col, ' + ', '.join(order_book_ids)
+    cols = 'index_col, ' + ', '.join(col_intercept)
 
     sql_str = 'SELECT {} FROM {} WHERE {} BETWEEN DATE(\'{}\') AND DATE(\'{}\')'.format(
         cols, factor_name, 'index_col', start_date, end_date)
